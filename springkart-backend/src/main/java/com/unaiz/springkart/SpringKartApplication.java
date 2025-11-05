e83a7ba623892616a0ee2cf396c59606e46d3241
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-11-05 15:26:16 +0530

    added startup log message for SpringKartApplication

--- springkart-backend/src/main/java/com/unaiz/springkart/SpringKartApplication.java
+++ springkart-backend/src/main/java/com/unaiz/springkart/SpringKartApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringKartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringKartApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKartApplication.class, args);
+
+        //startup message
+        System.out.println("SpringKartApplication started Successfully!");
+    }
 
 }
