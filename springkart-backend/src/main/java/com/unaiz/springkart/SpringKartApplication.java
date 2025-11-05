eeeaa067ea638fc28a820360e2597e050ce14a92
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-11-05 15:17:47 +0530

    First commit - project setup

---
+++ springkart-backend/src/main/java/com/unaiz/springkart/SpringKartApplication.java
@@ -0,0 +1,13 @@
+package com.unaiz.springkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKartApplication.class, args);
+	}
+
+}
